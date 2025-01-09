ecaace8ae514dd2eb07c9c3652ea84b1dcf1fc18
Priya Dumka <118343635+Priyadumka03@users.noreply.github.com>
2025-01-09 11:35:48 +0530

    Create Reverse linked list.cpp

---
+++ Reverse linked list.cpp
@@ -0,0 +1,50 @@
+/** Reverse Linked List (recursive)  
+same for recursive and iterative tc O(n) sc O(1)
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head== NULL || head->next==NULL){
+            return head;
+        }
+        ListNode* newhead=reverseList(head->next);
+        ListNode* front=head->next;
+        front->next= head;
+        head->next=NULL;
+     return newhead;  
+    }
+   
+};
+//iterative
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* temp=head;
+        ListNode* prev=NULL;
+        while(temp!=NULL){
+            ListNode* front=temp->next;
+            temp->next=prev;
+            prev=temp;
+            temp=front;
+        }
+        return prev;
+    }
+};
