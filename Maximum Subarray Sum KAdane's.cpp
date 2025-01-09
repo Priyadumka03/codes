6275bce9e0740594f9cf2dc2eaf90fa90719aadd
Priya Dumka <118343635+Priyadumka03@users.noreply.github.com>
2025-01-09 09:48:35 +0530

    Create Maximum Subarray Sum KAdane's.cpp

---
+++ Maximum Subarray Sum KAdane's.cpp
@@ -0,0 +1,87 @@
+brute force tc= near to O(n^3)
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxSubarraySum(int arr[], int n) {
+    int maxi = INT_MIN; // maximum sum
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            // subarray = arr[i.....j]
+            int sum = 0;
+
+            //add all the elements of subarray:
+            for (int k = i; k <= j; k++) {
+                sum += arr[k];
+            }
+
+            maxi = max(maxi, sum);
+        }
+    }
+
+    return maxi;
+}
+
+int main()
+{
+    int arr[] = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int maxSum = maxSubarraySum(arr, n);
+    cout << "The maximum subarray sum is: " << maxSum << endl;
+    return 0;
+}
+better n^2
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxSubarraySum(int arr[], int n) {
+    int maxi = INT_MIN; // maximum sum
+
+    for (int i = 0; i < n; i++) {
+        int sum = 0;
+        for (int j = i; j < n; j++) {
+            // current subarray = arr[i.....j]
+
+            //add the current element arr[j]
+            // to the sum i.e. sum of arr[i...j-1]
+            sum += arr[j];
+
+            maxi = max(maxi, sum); // getting the maximum
+        }
+    }
+
+    return maxi;
+}
+
+int main()
+{
+    int arr[] = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int maxSum = maxSubarraySum(arr, n);
+    cout << "The maximum subarray sum is: " << maxSum << endl;
+    return 0;
+}
+
+optimal O(n) sc=O(1)
+Kadane's Algorithm
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum=0;
+        int maxsum=INT_MIN;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            if(maxsum<sum){
+                maxsum=sum;
+            }
+            if(sum<0)
+            {
+                sum=0;
+            }
+        }
+        return maxsum;
+    }
+};
+
+
